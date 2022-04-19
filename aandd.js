93ef17c7d5d2493ac7e833057574e0bed8c6900c
haruta <a8120203@gsuite.si.aoyama.ac.jp>
2022-04-19 17:49:44 +0900

    1-1できました

---
+++ aandd.js
@@ -0,0 +1,12 @@
+function show(array) {
+    var html = '';
+    html = html + '<table border="1">';
+    html = html + '<tr>';
+    for (var i = 0; i < array.length; i++) {
+        html = html + '<td>' + array[i] + '</td>';
+    }
+    html = html + '</tr>';
+    html = html + '</table>';
+    //document.getElementById('res').innerHTML = html;
+    document.getElementById('res').insertAdjacentHTML('beforeend', html);
+}
