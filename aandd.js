d501c0eb4011c37314edaeb3af528d77e7d1a75e
haruta <a8120203@gsuite.si.aoyama.ac.jp>
2022-05-10 18:45:51 +0900

    ３終了

--- aandd.js
+++ aandd.js
@@ -1,14 +1,20 @@
-function show(array) {
-    var html = '';
-    html = html + '<table border="1">';
-    html = html + '<tr>';
-    for (var i = 0; i < array.length; i++) {
-        html = html + '<td>' + array[i] + '</td>';
+function show(obj) {
+    if (Array.isArray(obj)) {
+        var html = '';
+        html = html + '<table border="1">';
+        html = html + '<tr>';
+        for (var i = 0; i < obj.length; i++) {
+            html = html + '<td>' + obj[i] + '</td>';
+        }
+        html = html + '</tr>';
+        html = html + '</table>';
+        //html = html + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', html);
+    } else {
+        var string = JSON.stringify(obj);
+        string = string + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', string);
     }
-    html = html + '</tr>';
-    html = html + '</table>';
-    //document.getElementById('res').innerHTML = html;
-    document.getElementById('res').insertAdjacentHTML('beforeend', html);
 }
 
 function createSequentialNumbers(array, n) {
